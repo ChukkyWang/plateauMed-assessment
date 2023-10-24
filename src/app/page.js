b83a5f0f42995711430de3cf2ded88f222a33094
heyChukwuka <63220512+ChukkyWang@users.noreply.github.com>
2023-10-24 23:32:15 +0100

    Restructure App

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,10 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-
-      Welcome Home
-    </main>
+    <main className={styles.main}>Welcome Home</main>
   );
 }
